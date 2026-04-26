fe3ffa066b213d81e5d8baa34b79645f8c9eb70f
inadequacy <cole.brookslyon@gmail.com>
2026-04-26 02:07:51 +0200

    mostly changing scripts to supress warnings

---
+++ Assets/Scripts/Player_Character.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player_Character : MonoBehaviour {
+    public int health = 100;
+    public int lossRate = 1;
+
+    void Start()
+    {
+        //empty
+    }
+
+    void Update()
+    {
+        //empty
+    }
+}
